d9b489c1a7e9275f7cf8fba5cd0068f6b4836c8c
RishabhJM <74768997+RishabhJM@users.noreply.github.com>
2021-03-13 11:17:46 +0530

    Update c3py.py

--- c3py.py
+++ c3py.py
@@ -1,6 +1,6 @@
 import random
-a=random.randint(1,100)
-b=int(input('enter an integer between 1 and 99: '))
+a=random.randint(1,101)
+b=int(input('enter an integer between 1 and 100: '))
 if a==b:
     print('entered integer is correct')
 else:
